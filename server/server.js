a62b93ab63434302dbd8cc4425cfbbb98adc1e10
SanjayRcas <sanjaygrcas@gmail.com>
2025-07-02 18:48:46 +0530

    changes made in server.js in that cors

--- server/server.js
+++ server/server.js
@@ -12,7 +12,7 @@ const port = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors({
-  origin: 'http://localhost:3000'
+  origin: 'https://onlineshopping-1-mernstack-sanjay.netlify.app'
 }));
 
 app.use(express.json()); // allows us to parse incoming requests: req.body
